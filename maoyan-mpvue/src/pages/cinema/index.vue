700d84d9b6162901d781199ac995d875dd5eda9b
mystery66 <1317671142@qq.com>
2018-07-21 22:39:27 +0800

    完成搜索栏

---
+++ maoyan-mpvue/src/pages/cinema/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="cinema">
+    <head-top>
+      <div  slot="search">
+        <a  class="search-cinema" href="/pages/citie">
+          <span >
+            <img src="/static/images/icon-search-white.png">
+              找影院
+          </span>
+        </a>
+      </div>
+    </head-top>
+  </div>
+</template>
+
+<script>
+import HeadTop from '@/components/common/HeadTop'
+export default {
+  components: {
+    HeadTop
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.cinema
+   background-color #f5f5f5
+.search-cinema 
+  background-color #ffffff
+  width 75%
+  display inline-block
+  margin 20rpx 20rpx 20rpx 30rpx
+  height 48rpx
+  width 75%
+  border 1px solid #d7d7d7 
+  border-radius 10rpx
+  padding-top 9rpx
+  text-align center
+  img 
+    width 30rpx
+    height 30rpx
+    
+  
+    
+   
+   
+    
+</style>
---
+++ maoyan-mpvue/src/pages/cinema/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './index'
+
+export default {
+  config: {
+  "navigationBarTitleText": '影院',
+  }
+}
+const app = new Vue(App)
+app.$mount()
