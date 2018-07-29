3ecf4f358a66909f0e937185ec5a9750d6dac57f
mystery66 <1317671142@qq.com>
2018-07-30 07:17:08 +0800

    全局配置flyio

--- maoyan-mpvue/src/main.js
+++ maoyan-mpvue/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App'
+import Fly from 'flyio/dist/npm/wx'
 
+var fly = new Fly
+Vue.prototype.$http = fly
 
 Vue.config.productionTip = false
 App.mpType = 'app'
