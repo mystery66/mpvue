333041393a7721657dcbb0c46ef29e8fc3c48b2a
mystery66 <1317671142@qq.com>
2018-08-01 09:29:46 +0800

    详情页

---
+++ maoyan-mpvue/src/components/moviedetails/DetailsHead.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class='movie-head'>
+    <div class="head-content" :style="{backgroundImage: 'url(' +head.img+ ')' }">
+      <div class="head-img">
+        <img :src="head.img" >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+props: ['head'],
+
+}
+</script>
+
+<style lang="stylus" scoped>
+.movie-head
+  height 450rpx
+  color #333333
+</style>
