8614b9c72d350c91ecba37a0b82089632321bd87
mystery66 <1317671142@qq.com>
2018-08-02 23:43:55 +0800

    详情页头部未完成

--- maoyan-mpvue/src/components/moviedetails/DetailsHead.vue
+++ maoyan-mpvue/src/components/moviedetails/DetailsHead.vue
@@ -1,9 +1,15 @@
 <template>
   <div class='movie-head'>
-    <div class="head-content" :style="{backgroundImage: 'url(' +head.img+ ')' }">
-      <div class="head-img">
-        <img :src="head.img" >
+    <div class="head-bg" :style="{backgroundImage: 'url(' +head.img+ ')' }"></div>
+    <div class="head-content">
+      <div class="head-poster">
+        <img class="movie-img" :src="head.img" >
+        <a href="#">
+          <img  class="movie-playicon" src="/static/images/icon-play.png" >
+        </a>
+        
       </div>
+      
     </div>
   </div>
 </template>
@@ -16,7 +22,32 @@ props: ['head'],
 </script>
 
 <style lang="stylus" scoped>
-.movie-head
-  height 450rpx
-  color #333333
+.movie-head 
+  position relative
+  background-color #333333
+  .head-bg
+    height 450rpx
+    
+    opacity 0.5
+    filter:blur(40rpx); //滤镜 blur 高斯模糊
+    background-size cover
+
+  .head-content 
+    position absolute
+    padding 25rpx 30rpx 22rpx 30rpx
+    top 0
+    left 0
+    right 0
+    bottom 0
+    .head-poster
+      position relative
+      .movie-img
+        width 220rpx
+        height 300rpx  
+      .movie-playicon
+        position absolute
+        width 80rpx
+        height 80rpx
+        top 113rpx
+        left 70rpx
 </style>
