20652d1b8c94600f72eaa9b8ad520e8c015f7649
mystery66 <1317671142@qq.com>
2018-08-05 23:37:29 +0800

    添加了egg组件

--- maoyan-mpvue/src/pages/details/index.vue
+++ maoyan-mpvue/src/pages/details/index.vue
@@ -1,44 +1,52 @@
 <template>
   <div class="movie-details">
     <details-head :head="head"></details-head>
+    <div v-if="details.eggnum">
+      <movie-egg :eggnum="details.eggnum"></movie-egg>
+    </div>
+    
   </div>
 </template>
 
 <script>
 import DetailsHead from '@/components/moviedetails/DetailsHead'
+import MovieEgg from '@/components/moviedetails/MovieEgg'
 export default {
 components: {
-  'details-head': DetailsHead
+  'details-head': DetailsHead,
+  'movie-egg': MovieEgg
 },
 data () {
   return {
     details: [],
-    head:[],
-    id:null
+    head:[]
+    
   }
 },
-onLoad: function(options) {
-  this.id = options.id;
+onLoad (options) {
+ let id = options.id;
   console.log(options);
-},
-onShow: function (options) {
-  console.log(options);
-  let id = this.id;
-  console.log(id);
-  this.$http.get('https://www.easy-mock.com/mock/5b5fae909f3d1d7bfe3a6f8a/details/#!method=get')
+   this.$http.get('https://www.easy-mock.com/mock/5b5fae909f3d1d7bfe3a6f8a/details/#!method=get')
     .then((res) => {
       const data= res.data.data.details.filter((item) => {
-        return item.id == id
+       return item.id == id
       })
       // console.log(data);
       this.details = data[0];
       this.head = this.details.head
-      console.log(this.details.head);
+      console.log(this.details.head.img);
     })
-}
+},
+
 }
 </script>
 
-<style>
+<style lang="stylus" scoped>
+.movie-details
+  background-color #f5f5f5
+  
+
+
+
 
 </style>
